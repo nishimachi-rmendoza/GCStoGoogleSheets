abdf32c34148e63e40fd162f758b54e2335545fe
nishimachi-rmendoza <165972058+nishimachi-rmendoza@users.noreply.github.com>
2024-04-04 11:11:33 +0900

    Update importCSVfromGCS.js

--- importCSVfromGCS.js
+++ importCSVfromGCS.js
@@ -1,9 +1,10 @@
 function importCSVFromGCS() {
   // Set your variables here
   // This should be the only section that needs edits
+  // These variables don't need to be obfuscated. Protect your data via roles directly in your GCP project instead.
   var bucketName = 'insertbucketnamehere';
   var fileName = 'foldername/filename'; // The structure here is folder/filename. Look for the exact filename in GCS as it may contain an extension
-  var sheetName = 'Main'; // The name of the sheet where you want to import the data
+  var sheetName = 'Main'; // The name of the sheet (tab) where you want to import the data
   
   // Fetch the CSV file from the GCS bucket
   var url = 'https://storage.googleapis.com/' + bucketName + '/' + fileName;
@@ -33,7 +34,7 @@ function importCSVFromGCS() {
   sheet.getRange(1, 1, csvRows.length, csvRows[0].length).setValues(csvRows);
 
   // Add the "Time Synced" column immediately after the imported data
-  // This is so that you can know exactly when the last sync was directly from the sheet
+  // This adds a final row to the sheet in order to tell when the last export was directly from the sheet
   // Don't forget to make a trigger after testing this!
   var currentTime = new Date();
   var formattedTime = Utilities.formatDate(currentTime, Session.getScriptTimeZone(), "yyyy-MM-dd'T'HH:mm:ss'Z'");
@@ -41,4 +42,5 @@ function importCSVFromGCS() {
   sheet.getRange(2, numColumnsToClear).setValue(formattedTime); // Set timestamp
 
   //Rod Mendoza @ Nishimachi International School
+  //rmendoza@nishimachi.ac.jp
 }
